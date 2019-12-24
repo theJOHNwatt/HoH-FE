f3b381a92ad7bb8e549078979ca47e68cd44ff69
John Watt <j.watt10@gmail.com>
2019-12-23 17:07:45 -0700

    :sparkles: File Structure and Components for nav
    Added react router dom and created routes to lounge and store. Created folders to house lounge, store, and nav components.

--- hoh/src/App.js
+++ hoh/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from 'react'
+import {Route} from 'react-router-dom'
+
+import LoungeHome from './components/lounge/LoungeHome'
+import StoreHome from './components/store/StoreHome'
+import {Link} from 'react-router-dom'
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <h1>ITS ALIVE</h1>
+    
+    <div className='front-page-links'>
+      <Link to='/lounge'><h2>Lounge</h2></Link> 
+      <Link to='/store'><h2>Store</h2></Link>
+    </div>
+      
+
+    <Route  exact path='/lounge' component={LoungeHome} />
+    <Route exact path='/store' component={StoreHome} />
+
     </div>
   );
 }
---
+++ hoh/src/components/lounge/LoungeHome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LoungeHome() {
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default LoungeHome;
